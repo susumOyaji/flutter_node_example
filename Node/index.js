e8c404643646ff17cb68039c92ead21e36e660cf
susumOyaji <sumitomo0210@gmail.com>
2023-03-25 10:57:07 +0900

    up

---
+++ Node/index.js
@@ -0,0 +1,42 @@
+
+//Hello World の例
+//ここで紹介するのは基本的に、作成できる最も単純な Express アプリケーションです。このアプリケーションは単一ファイル・アプリケーションであり、Express ジェネレーター を使用して得られるものでは ありません 。このジェネレーターは、さまざまな目的で多数の JavaScript ファイル、Jade テンプレート、サブディレクトリーを使用する完全なアプリケーション用のスキャフォールディングを作成します。
+
+//最初に、myapp という名前のディレクトリーを作成して、そのディレクトリーに移動し、npm init を実行します。次に、インストール・ガイドに従い、依存関係として express をインストールします。
+
+//myapp ディレクトリーで、app.js というファイルを作成して、以下のコードを追加します。
+
+
+const express = require('express')
+// expressアプリを生成する
+const app = express()
+const port = 3000
+
+
+// webフォルダの中身を公開する
+app.use(express.static('./Node/'));
+
+
+app.get('/api/v1/list', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+//アプリケーションは、サーバーを始動して、ポート 3000 で接続を listen します。
+//アプリケーションは、ルート URL (/) またはルート に対する要求に「Hello World!」と応答します。
+//その他すべてのパスについては、「404 Not Found」と応答します。
+
+//req (要求) と res (応答) は、Node が提供するのとまったく同じオブジェクトであるため、Express が関与しない場合と同じように、req.pipe()、req.on('data', callback) などを呼び出すことができます。
+
+//次のコマンドを使用してアプリケーションを実行します。
+
+//$ node app.js
+//次に、ブラウザーに http://localhost:3000/ をロードして、出力を確認します。
+
+
+
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,12 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   Future<Map<String, dynamic>> fetchData() async {
+    //localhost:3000/api/v1/listにGETリクエストを送信し、レスポンスを待ちます。
+    //レスポンスが受信されると、response変数に代入されます。
     final response =
         await http.get(Uri.parse('http://localhost:3000/api/v1/list'));
+    //await http.get(Uri.parse('http://192.168.2.111:3000/endpoint'));//192.168.2.111.3000
+    //await http.get(Uri.parse('https://api.cashless.go.jp'));
 
     if (response.statusCode == 200) {
       return json.decode(response.body);
@@ -30,8 +34,8 @@ class MyApp extends StatelessWidget {
           builder: (context, snapshot) {
             if (snapshot.hasData) {
               return Center(
-                  child: Text(snapshot.data?['message']),
-                  );
+                child: Text(snapshot.data?['message']),
+              );
             } else if (snapshot.hasError) {
               return Center(
                 child: Text('${snapshot.error}'),
