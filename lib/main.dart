3344892d20abc9e3866d0a874a5e03783835ffe9
susumOyaji <sumitomo0210@gmail.com>
2023-03-25 09:04:29 +0900

    up

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  Future<Map<String, dynamic>> fetchData() async {
+    final response =
+        await http.get(Uri.parse('http://localhost:3000/api/v1/list'));
+
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to fetch data');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: FutureBuilder<Map<String, dynamic>>(
+          future: fetchData(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Center(
+                  child: Text(snapshot.data?['message']),
+                  );
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text('${snapshot.error}'),
+              );
+            }
+
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
